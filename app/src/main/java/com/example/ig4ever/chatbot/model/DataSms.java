efedf97c6fdef4897c8df92c50ac494efa9b3b3f
rakhmat <rahmatig4ever@gmail.com>
2017-05-04 07:51:30 +0700

    chatbot template

---
+++ app/src/main/java/com/example/ig4ever/chatbot/model/DataSms.java
@@ -0,0 +1,27 @@
+package com.example.ig4ever.chatbot.model;
+
+/**
+ * Created by ig4ever on 5/3/2017.
+ */
+
+public class DataSms {
+
+    private String pesan;
+    private String waktu;
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public void setPesan(String pesan) {
+        this.pesan = pesan;
+    }
+
+    public String getWaktu() {
+        return waktu;
+    }
+
+    public void setWaktu(String waktu) {
+        this.waktu = waktu;
+    }
+}
